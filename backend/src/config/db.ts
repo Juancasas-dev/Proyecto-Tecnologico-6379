358480c6db46a4c9d11a38c9963d8a324f9c28fd
Juan Casas <juancasasherrera@gmail.com>
2026-04-21 19:37:26 -0500

    inicio de login en back

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI as string);
-    console.log("Conexión a MongoDB exitosa");
+    console.log("MongoDB conectado");
   } catch (error) {
     console.error("Error conectando a MongoDB:", error);
     process.exit(1);
