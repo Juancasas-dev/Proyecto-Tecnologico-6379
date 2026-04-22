278805422f6567c6e761c4029d1dd1ba57a896c9
Juan Casas <juancasasherrera@gmail.com>
2026-04-21 19:05:47 -0500

    Previo al login

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI as string);
-    console.log("✅ Conexión a MongoDB exitosa");
+    console.log("Conexión a MongoDB exitosa");
   } catch (error) {
-    console.error("❌ Error conectando a MongoDB:", error);
+    console.error("Error conectando a MongoDB:", error);
     process.exit(1);
   }
 };
