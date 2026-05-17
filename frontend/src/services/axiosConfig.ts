e784d7df62b441c51b5914c28871780a713affc0
Juan Casas <juancasasherrera@gmail.com>
2026-05-16 21:54:11 -0500

    Validaciones y formularios,email credenciales

---
+++ frontend/src/services/axiosConfig.ts
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+axios.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('token')
+      localStorage.removeItem('usuario')
+      setTimeout(() => {
+        window.location.href = '/login?expired=true'
+      }, 500)
+    }
+    return Promise.reject(error)
+  }
+)
